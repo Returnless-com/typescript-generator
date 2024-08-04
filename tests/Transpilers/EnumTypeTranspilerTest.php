dee4b4e832503620685804b952668c703f104224
Jelle Spekken <40019+jspekken@users.noreply.github.com>
2024-08-05 00:06:01 +0200

    fix: add missing strict-types to stub classes

--- tests/Transpilers/EnumTypeTranspilerTest.php
+++ tests/Transpilers/EnumTypeTranspilerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Returnless\TypescriptGenerator\Tests\Transpilers;
 
 use PHPUnit\Framework\Attributes\Test;
--- tests/stubs/BackedEnumStub.php
+++ tests/stubs/BackedEnumStub.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Returnless\TypescriptGenerator\Tests\stubs;
 
 enum BackedEnumStub: string
